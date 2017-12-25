4c30abcbef934d6bb7807352fd1393b5b54866a3
Joooooooye <ruh@zju.edu.cn>
2017-12-25 20:06:34 +0800

    services

--- www/js/services.js
+++ www/js/services.js
@@ -19,8 +19,7 @@
 
     .constant('CONFIG', {
       baseUrl: 'http://localhost:1420/api/', // RESTful 服务器
-      localUrl: 'E:/1实验室/9 深圳863/深圳863—知识库/OWL and Rules/',
-      // localUrl:'/Users/ruh/Desktop/319/863/深圳863—知识库/OWL and Rules',
+      localUrl: 'E:/1实验室/9 深圳863项目结题/深圳863—知识库/OWL and Rules/',
       model: 'RMDK v1.2.owl'
 
         /* List all the roles you wish to use in the app
@@ -50,7 +49,8 @@
           path: 'InfoInput'
         }, {
             // 向患者输入数据属性，即添加患者信息,输入：string guid, string value, string property
-          addPatProperty: { method: 'POST', params: { route: 'addObjProperty', guid: '@guid' }, timeout: 10000 }
+          addPatProperty: { method: 'POST', params: { route: 'addObjProperty', guid: '@guid' }, timeout: 10000 },
+          createPat: { method: 'POST', params: { route: 'crePatient' }, timeout: 10000 }
 
         })
       }
@@ -73,20 +73,20 @@
             // 推理分析患者需要参与的医学检查，医学检查为检查组合名称，需要调用ExamInfo方法显示具体的检查项目。
           examRecommend: { method: 'GET', params: { route: 'ExamRecGen', guid: '@guid' }, timeout: 1000 },
           // 显示具体的医学检查项目，对接ExamRecGen方法。通过ExamRecGen方法得到的检查组，输入到ExamInfo方法中，得出患者具体需要参加的医学检查项目，结果分类显示。
-          // examInfo: { method: 'POST', params: { route: 'ExamInfo ', list: '@list'}, timeout: 1000 },
+          examInfo: { method: 'POST', params: { route: 'ExamInfo', list: '@list'}, timeout: 1000 },
           // 推理分析患者需要参与的疾病筛查，筛查为检查组合名称，需要调用ScreenInfo方法显示具体的检查项目。
-          screenRecommend: { method: 'GET', params: { route: 'ScreenRecGen ', guid: '@guid'}, timeout: 1000 },
+          screenRecommend: { method: 'GET', params: { route: 'ScreenRecGen', guid: '@guid'}, timeout: 1000 },
           // 显示具体的筛查所需检查项目，注意事项，和筛查周期，对接ScreenRecGen方法。通过ScreenRecGen方法得到筛查组，输入到ScreenInfo方法中，得出患者具体需要参加的检查项目、注意事项，以及筛查周期。
-          screenInfo: { method: 'GET', params: { route: 'ScreenInfo ', ScreenIn: '@ScreenIn'}, timeout: 1000 }
+          screenInfo: { method: 'GET', params: { route: 'ScreenInfo', ScreenIn: '@ScreenIn'}, timeout: 1000 }
         })
       }
       // 用药建议
       var MedicationRec = function () {
         return $resource(CONFIG.baseUrl + ':path/:route', { path: 'MedRec' }, {
           // 推理建议使用的药物。结果为药物在本体知识库中的名称，需要调用DrugInfo方法获得药物的具体信息。结果分类返回。
-          drugsRec: { method: 'GET', params: { route: 'DrugProvider ', guid: '@guid' }, timeout: 1000 },
+          drugsRec: { method: 'GET', params: { route: 'DrugProvider', guid: '@guid' }, timeout: 1000 },
           // 查询药物具体信息，包括名称、类型、用量、注意事项等，对接DrugProvider方法。需要将DrugProvider得到的DList、DListA和DListC输入到DrugInfo中，得到药物信息。
-          drugsInfo: { method: 'POST', params: { route: 'DrugInfo ' }, timeout: 1000 }
+          drugsInfo: { method: 'POST', params: { route: 'DrugInfo' }, timeout: 1000 }
 
         })
       }
@@ -175,6 +175,15 @@
         })
         return deferred.promise
       }
+      self.createPat = function (obj) {
+        var deferred = $q.defer()
+        Data.InfoInput.createPat(obj, function (data, headers) {
+          deferred.resolve(data)
+        }, function (err) {
+          deferred.reject(err)
+        })
+        return deferred.promise
+      }
 
       return self
     }])
@@ -226,15 +235,15 @@
         })
         return deferred.promise
       }
-      // var examInfo = function (obj) {
-      //   var deferred = $q.defer()
-      //   Data.ExamRecommended.examInfo(obj, function (data, headers) {
-      //     deferred.resolve(data)
-      //   }, function (err) {
-      //     deferred.reject(err)
-      //   })
-      //   return deferred.promise
-      // }
+      var examInfo = function (obj) {
+        var deferred = $q.defer()
+        Data.ExamRecommended.examInfo(obj, function (data, headers) {
+          deferred.resolve(data)
+        }, function (err) {
+          deferred.reject(err)
+        })
+        return deferred.promise
+      }
       var screenRecommend = function (obj) {
         var deferred = $q.defer()
         Data.ExamRecommended.screenRecommend(obj, function (data, headers) {
@@ -258,18 +267,38 @@
         var deferred = $q.defer()
         examRecommend({guid: guid}).then(function (data) {
           var examGroups = data.ExamRec
-          $.ajax({
-            url: CONFIG.baseUrl + 'ExamRec/ExamInfo',
-            type: 'POST',
-            data: data.ExamRecNode,
-            dataType: 'json',
-            success: function (data) {
-              console.log(data)
-              // 卡住了
-            }
-          })
         })
       }
+      // screenRecommend和screenInfo对接
+      self.getScreenRec = function (guid) {
+        var deferred = $q.defer()
+        screenRecommend({guid: guid}).then(function (data) {
+          // console.log(data)
+          // var screenGroups = data.ScreenRec
+          var promiseList = []
+          data.ScreenRecNode.forEach(function (value) {
+            promiseList.push(screenInfo({ScreenIn: value}))
+          })
+          $q.all(promiseList).then(function (res) {
+            res.forEach(function (value) {
+              value['ScreenRec'] = data.ScreenRec.shift()
+              value.LabExam = value.LabExam.join(', ')
+              value.PhyExam = value.PhyExam.join(', ')
+              value.SpeExam = value.SpeExam.join(', ')
+              value.LabExamOp = value.LabExamOp.join(', ')
+              value.PhyExamOp = value.PhyExamOp.join(', ')
+              value.SpeExamOp = value.SpeExamOp.join(', ')
+            })
+            deferred.resolve(res)
+          }, function (err) {
+            deferred.reject(err)
+          })
+        }, function (err) {
+          deferred.reject(err)
+        })
+        return deferred.promise
+      }
+
       return self
     }])
     // 药物建议
